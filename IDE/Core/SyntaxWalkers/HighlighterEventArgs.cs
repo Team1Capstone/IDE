b42e156738dc97e171fc073125e52437c5b4fd7c
Kirk Dietz <kdietz1@email.davenport.edu>
2014-11-19 18:33:37 -0500

    Added HightlighterEventArgs.cs

---
+++ IDE/Core/SyntaxWalkers/HighlighterEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Core.Text;
+
+namespace Core.SyntaxWalkers
+{
+    public class HighlighterEventArgs : EventArgs
+    {
+        public List<ColorTextSpan> Changes;
+
+        public HighlighterEventArgs() {
+            Changes = new List<ColorTextSpan>();
+        }
+
+        public HighlighterEventArgs(IEnumerable<ColorTextSpan> changes) : this()
+        {
+            Changes.AddRange(changes);
+        }
+    }
+}
